93658b9d8b1bd43227a364f2c5a14c4102409087
jason <jason_xxx7@163.com>
2020-02-29 17:05:58 +0800

    5.菜单导航图标渲染完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,21 +1,36 @@
-import requset from "../../utils/request.js";
+import request from "../../utils/request.js";
 
 Page({
 
 data:{
-//轮播图
-banners:[]
+   //轮播图
+   banners:[],
+
+   //菜单图标数据
+   menus:[]
 },
 onLoad(){
   //请求轮播图接口
-  requset({
+  request({
     url:"/home/swiperdata"
   }).then(res=>{
     //在res.data解构message轮播图数组
     const {message}=res.data;
-    console.log(message)
+    // console.log(message)
     this.setData({
       banners:message
+    })
+  }),
+
+  //请求菜单图标接口
+  request({
+    url: '/home/catitems',
+  }).then(res=>{
+    console.log(res)
+    //在res.data解构message菜单栏数组
+    const{message}=res.data;
+    this.setData({
+      menus:message
     })
   })
 }
