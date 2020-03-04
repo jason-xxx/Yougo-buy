ac61a44c760bf81e3e0a8a02789ebd6c9101475c
jason <jason_xxx7@163.com>
2020-03-04 21:36:48 +0800

    27.1控制请求接口函数的执行频率（防抖和节流1.0）
    不让搜索搜索框一监控值的变化就进行请求，而是等上一个请求结束之后再请求

--- pages/search/index.js
+++ pages/search/index.js
@@ -8,7 +8,9 @@ Page({
    data: {
       inputValue:'',
       // 搜索建议
-      recommend:[]
+      recommend:[],
+      //设置一个开关座节流防抖的 “开关”
+      loading:false
    },
 
    /**
@@ -38,19 +40,31 @@ Page({
 
    //将上面的获取搜索建议请求封装起来
    getRecommend(){
-      request({
-         url: '/goods/qsearch',
-         data: {
-            query: this.data.inputValue//封装后这个要改
-         }
-      }).then(res => {
-         // console.log(res)
-         const { message } = res.data;
-         //保存搜索建议
-         this.setData({
-            recommend: message
+      
+      //如果开关是关着的就发出请求
+      if(this.data.loading===false){
+
+        //通过loading=false进来后，就把loading开关改为true
+        this.setData({
+           loading:true
+        })
+
+         request({
+            url: '/goods/qsearch',
+            data: {
+               query: this.data.inputValue//封装后这个要改
+            }
+         }).then(res => {
+            // console.log(res)
+            const { message } = res.data;
+            //保存搜索建议
+            this.setData({
+               recommend: message,
+               //请求完成后将开关重新改为false,从而做到节流防抖作用
+               loading:false
+            })
          })
-      })
+      }
    },
    //点击取消按钮的事件，
    handleCancel(){
