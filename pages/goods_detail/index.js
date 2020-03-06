b3b7cc5c1be759796b24a03a1cd2b05feb674b11
jason <jason_xxx7@163.com>
2020-03-07 00:26:35 +0800

    41.把商品添加到本地的购物车

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -69,5 +69,38 @@ Page({
       wx.switchTab({
          url: '/pages/cart/index',
       })
+   },
+   //点击加入购物车按钮
+   handleAddCart() {
+      //保存到本地，先判断本地是否有整个大数组，如果没有就创立一个空数组
+      const goods = wx.getStorageSync('goods') || []
+      //使用some的数组方法判断本地goods数组里是否有这个id的商品商品，
+      //如果有商品就number(商品数量)+1,假如本地goods数组没有，就将相关数据unshift保存到数组
+      const exit = goods.some(v => {
+         //这里通过some的数组方法利用商品的id（因为是唯一值）判断
+         const isExit = v.goods_id === this.data.detail.goods_id;
+         //  假如本地goods数组有这个id的商品，number+1
+         if (isExit) {
+            v.number += 1;
+            //弹出提示框，文档=>界面=》界面=》交互=》第一个
+            wx.showToast({
+               title: '数量+1',
+               icon: "success"
+            })
+         }
+         return isExit;
+      })
+      //假如本地goods数组没有这个id的商品，就将相关数据unshift保存到数组（some方法为false）
+      if (!exit) {
+         goods.unshift({
+            goods_id: this.data.detail.goods_id,
+            goods_name: this.data.detail.goods_name,
+            goods_price: this.data.detail.goods_price,
+            goods_small_logo: this.data.detail.goods_small_logo,
+            number: 1
+         })
+      }
+      //将goods保存到本地
+      wx.setStorageSync("goods", goods)
    }
 })
